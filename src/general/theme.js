d19b7264aaaf589a0e555960a664c99c5368c751
David Rubin <dudrubin@gmail.com>
2019-11-16 13:35:35 +0200

    Added styled components

---
+++ src/general/theme.js
@@ -0,0 +1,4 @@
+export const theme = {
+    mainColor:'blue',
+    secondaryColor:'green'
+}
---
+++ src/shared/Components/Input/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { StyledInput } from "./styles";
+
+export const Input  = (props={})=>  <StyledInput {...props} />
